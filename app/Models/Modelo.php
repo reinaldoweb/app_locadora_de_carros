81c265ba734824a779b57e1e04a6595b0f5bd675
reinaldodev <reinaldoweb@hotmail.com>
2022-08-17 15:21:09 -0300

    Implementação do Model modelo

--- app/Models/Modelo.php
+++ app/Models/Modelo.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Modelo extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['marca_id, nome', 'imagem', 'numero_portas', 'lugares', 'air_bags', 'abs'];
+
+    public function rules()
+    {
+        return [
+            'marca_id' => 'exixts:marcas,id',
+            'nome' => 'required|unique:modelos, ' . $this->id . '|min:3',
+            'imagem' => 'required|file|mimes:png, jpg,jpeg',
+            'numero_portas' =>'required|integer|digits_between:1,5',
+            'lugares' => 'required|integer|digits_between:1,20',
+            'air_bags' => 'required|boolean',
+            'abs' => 'required|boolean'
+        ];
+    }
 }
