047fd5c111f274c9bfd18f2634532c066da689ce
reinaldodev <reinaldoweb@hotmail.com>
2022-09-29 14:47:55 -0300

    Buscando registros

--- resources/js/components/Marcas.vue
+++ resources/js/components/Marcas.vue
@@ -12,21 +12,22 @@
                                 <input-container-component titulo="ID" id="inputId" id-help="idHelp"
                                     texto-ajuda="Opcional. Informe o ID da marca">
                                     <input type="number" class="form-control" id="inputId" aria-describedby="idHelp"
-                                        placeholder="ID">
+                                        placeholder="ID" v-model="busca.id">
                                 </input-container-component>
                             </div>
                             <div class="col mb-3">
                                 <input-container-component titulo="Nome da marca" id="inputNome" id-help="nomeHelp"
                                     texto-ajuda="Opcional. Informe o nome da marca">
                                     <input type="text" class="form-control" id="inputNome" aria-describedby="nomeHelp"
-                                        placeholder="Nome da marca">
+                                        placeholder="Nome da marca" v-model="busca.nome">
                                 </input-container-component>
                             </div>
                         </div>
                     </template>
 
                     <template v-slot:rodape>
-                        <button type="submit" class="btn btn-primary btn-sm float-right">Pesquisar</button>
+                        <button type="submit" class="btn btn-primary btn-sm float-right"
+                            @click="pesquisar()">Pesquisar</button>
                     </template>
                 </card-component>
                 <!-- fim do card de busca -->
@@ -126,22 +127,48 @@ export default {
     data() {
         return {
             urlBase: 'http://localhost:8000/api/v1/marca',
+            urlPaginacao: '',
+            urlFiltro: '',
             nomeMarca: '',
             arquivoImagem: [],
             transacaoStatus: '',
             transacaoDetalhes: {},
-            marcas: { data: [] }
+            marcas: { data: [] },
+            busca: { id: "", nome: "" }
         }
     },
     methods: {
+        pesquisar() {
+            // console.log(this.busca)
+            let filtro = ''
+            for (let chave in this.busca) {
+                if (this.busca[chave]) {
+                    // console.log(chave, this.busca[chave])
+
+                    if (filtro !== '') {
+                        filtro += ";"
+                    }
+                    filtro += chave+':like:'+this.busca[chave]
+                }
+            }
+            if (filtro != '') {
+                this.urlPaginacao = 'page1'
+                this.urlFiltro = '&filtro='+filtro
+            }else{
+                this.urlFiltro = ''
+            }
+            this.carregarLista()
+        },
         paginacao(l) {
             if (l.url) {
-                this.urlBase = l.url //Ajustgando a url com o parametro da pagina
+                // this.urlBase = l.url //Ajustgando a url com o parametro da pagina
+                this.urlPaginacao = l.url.split('?')[1]
                 this.carregarLista() // Requisitando novamente os dados para nossa API.
             }
         },
         carregarLista() {
 
+            let url = this.urlBase + '?' + this.urlPaginacao + this.urlFiltro
             let config = {
                 headers: {
                     'Accept': 'application/json',
@@ -149,7 +176,8 @@ export default {
                 }
             }
 
-            axios.get(this.urlBase, config)
+            console.log(url)
+            axios.get(url, config)
                 .then(response => {
                     this.marcas = response.data
                     console.log(this.marcas)
